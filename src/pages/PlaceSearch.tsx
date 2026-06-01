a1c7a3f29bef1d4f974293b658ca7d4c3db25dab
devspace <devspace@noreply.local>
2026-06-01 16:09:07 +0900

    fix: PlaceSearch 미사용 import 제거

--- src/pages/PlaceSearch.tsx
+++ src/pages/PlaceSearch.tsx
@@ -2,7 +2,7 @@ import { useMemo, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import TopBar from '../components/TopBar'
 import { SearchIcon, PinIcon } from '../components/Icons'
-import { search, buildings, indoorMapsOf, defaultFloor, type SearchHit } from '../lib/data'
+import { search, buildings, defaultFloor, type SearchHit } from '../lib/data'
 
 const PAGE = 7
 
