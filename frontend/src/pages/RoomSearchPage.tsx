e9ca59516f2f0bf72cb9e14ddf8ff29bfec8d11b
cylee127 <chang010207@naver.com>
2026-06-07 14:04:41 +0900

    [BE] Revert frontend changes from week 8 branch

--- frontend/src/pages/RoomSearchPage.tsx
+++ frontend/src/pages/RoomSearchPage.tsx
@@ -1,4 +1,4 @@
-import { useState, type FormEvent } from "react";
+import { FormEvent, useState } from "react";
 import { Link } from "react-router-dom";
 import { mapAssetUrl, searchRoom, type RoomSearchResult } from "../api/backend";
 
